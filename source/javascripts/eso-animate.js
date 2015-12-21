dc9e45b687fdf2d53d4733b921f236bfa13ed072
Emma <doodybrainz@gmail.com>
2015-12-21 08:58:55 -0500

    press section add

--- source/javascripts/eso-animate.js
+++ source/javascripts/eso-animate.js
@@ -17,6 +17,27 @@ var Eso = {
     });
 
     window.sr = new scrollReveal();
+
+    $('[data-behavior="show-bell"]').on('click', function() {
+      $('.show').hide();
+      $('.show.bell').show();
+    });
+    $('[data-behavior="show-judith"]').on('click', function() {
+      $('.show').hide();
+      $('.show.judith').show();
+    });
+    $('[data-behavior="show-randy"]').on('click', function() {
+      $('.show').hide();
+      $('.show.randy').show();
+    });
+    $('[data-behavior="show-iris"]').on('click', function() {
+      $('.show').hide();
+      $('.show.iris').show();
+    });
+    $('[data-behavior="read-more"]').on('click', function() {
+      $(this).next('ul').show();
+      $(this).hide();
+    });
   },
 
   resizeHandler: function() {
@@ -25,6 +46,28 @@ var Eso = {
 
     var module = $('.esoesoeso');
     module.css('height', EsoHeight);
+  },
+
+  scrollHandler: function() {
+    var dates = $('.dates').offset().top - 200,
+    lyricGrid = $('.lyric-grid').offset().top,
+    top = $(window);
+
+    if (top.scrollTop() >= dates ) {
+      $('.main-header').hide();
+      $('.show-header').show();
+      $('.secondary').hide();
+    } else {
+      $('.main-header').show();
+      $('.show-header').hide();
+      $('.secondary').hide();
+    }
+
+    if (top.scrollTop() >= lyricGrid ) {
+      $('.secondary').show();
+      $('.main-header').hide();
+      $('.show-header').hide();
+    }
   },
 
   shuffle: function () {
